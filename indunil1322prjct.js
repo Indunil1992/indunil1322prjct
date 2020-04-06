0fa7fa1c8175ecd31dad8b742fcef3d0efd8ed2f
Indunil1992 <indunil@adroitlogic.com>
2020-04-06 11:27:05 +0530

    2

--- indunil1322prjct.js
+++ indunil1322prjct.js
@@ -1,5 +1,19 @@
+let AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
+const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
- 
+
+    try {
+        let data = await sqs.sendMessage({
+            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/KTestSQS`,
+            MessageBody: "zx",
+            DelaySeconds: 0
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+
     return { "message": "Successfully executed" };
 };
